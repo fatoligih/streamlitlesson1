b292fe1ba673f6edf5bb37273c225231817f1748
fatoligih <46484325+fatoligih@users.noreply.github.com>
2022-09-20 12:58:29 +0700

    Add files via upload

---
+++ myfile.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+st.text("Tuần này mọi người đóng cho Cường nhé; chi tiết ở bảng dưới đây")
+
+st.image("https://i.imgur.com/rtRALUN.png")
+
+st.write("Ai đóng xong gạt nút dưới này nè")
+
+agree1 = st.checkbox('Huy')
+agree2 = st.checkbox('Thảo')
+agree3 = st.checkbox('Đức')
+agree4 = st.checkbox('Giang')
+agree5 = st.checkbox('Hà')
+agree6 = st.checkbox('Linh')
+
+st.title("Gièo cầu lông")
